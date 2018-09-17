1d654b6043d2d4c622e3e483ca0d5d53cbc38615
Tyler Montney <tyler.montney@potawatomibdc.com>
2018-09-17 13:24:42 -0500

    initial commit

---
+++ CatFactsNET/Core.vb
@@ -0,0 +1,8 @@
+﻿Module Core
+    Private ListenOnIP As String = Net.IPAddress.Loopback.ToString()
+    Private ListOnPort As Integer = 1337
+    Sub Main()
+        Dim contr As New Grapevine.Server.RestServer With {.Host = ListenOnIP, .Port = ListOnPort}
+        contr.LogToConsole().Start()
+    End Sub
+End Module
---
+++ CatFactsNET/Routes.vb
@@ -0,0 +1,21 @@
+﻿Public Class Routes
+    <Grapevine.Server.Attributes.RestResource>
+    Public Class Actions
+        <Grapevine.Server.Attributes.RestRoute(HttpMethod:=Grapevine.Shared.HttpMethod.GET, PathInfo:=Paths.CATFACT)>
+        Public Function CatFact(ByVal Context As Grapevine.Interfaces.Server.IHttpContext) As Grapevine.Interfaces.Server.IHttpContext
+            Dim serializer As New Web.Script.Serialization.JavaScriptSerializer
+            Dim CatFactsData As Object() = serializer.DeserializeObject(My.Resources.CatFacts)
+
+            Dim r As New Random()
+            Dim Index As Integer = r.Next(0, CatFactsData.Length - 1)
+
+            Context.Response.StatusCode = Grapevine.Shared.HttpStatusCode.Ok
+            Context.Response.SendResponse(Text.ASCIIEncoding.ASCII.GetBytes(CatFactsData(Index)))
+            Console.WriteLine("Generated random fact: " & CatFactsData(Index))
+            Return Context
+        End Function
+    End Class
+    Public Class Paths
+        Public Const CATFACT As String = "/API/CATFACT"
+    End Class
+End Class
